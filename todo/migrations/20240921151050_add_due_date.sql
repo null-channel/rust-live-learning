194a1c7c90e13932390962e43e7da1d00a54be22
Marek Counts <countsmarek@gmail.com>
2024-11-02 11:13:35 -0400

    added migration, and now we see those errors reflected in our code

--- todo/migrations/20240921151050_add_due_date.sql
+++ todo/migrations/20240921151050_add_due_date.sql
@@ -1,4 +1,5 @@
 -- Add migration script here
 ALTER TABLE todos ADD COLUMN due_date DATE;
 ALTER TABLE todos ADD COLUMN completion_date DATE;
-update todos set completion_date = '2024-09-21' where completed = 1;
+UPDATE todos set completion_date = '2024-09-21' where completed = 1;
+ALTER TABLE todos DROP COLUMN completed;
